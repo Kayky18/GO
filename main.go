cc3110c159440085f62ed2978c4252fb438272a2
Kayky <kayky.marinho18@gmail.com>
2024-09-06 13:15:35 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/kayky18/gopportunities/router"
+import (
+	"github.com/kayky18/gopportunities/config"
+	"github.com/kayky18/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	//Validate config
+	if err != nil {
+		logger.Errorf("Config err: %v", err)
+		return
+	}
+
 	//iniando o router
 	router.Initialize()
 }
